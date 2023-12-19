a38155ef929441ebb3e0c9c3beda00c84cf7fe28
Aina <ainafc3@gmail.com>
2023-12-19 14:57:06 +0100

    funciones y arrays

--- src/funcArray.js
+++ src/funcArray.js
@@ -1,16 +1,64 @@
-/* Encuentra el máximo */
+export function maxOfTwoNumbers(numero1, numero2) {
+    return (Math.max(numero1, numero2));
+}
 
-export function maxOfTwoNumbers(numero1,numero2){}
-export function findLongestWord(array){}
-export function sumArray(array){}
-export function averageNumbers(array){}
-export function averageWordLength(){}
-export function uniquifyArray(){}
-export function doesWordExist(array, palabra){}
-export function howManyTimes(){}
-export function greatestProduct(){}
+export function findLongestWord(array) {
+    if (array.length === 0) {
+        return undefined;
+    }
+    let longestWord = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i].length > longestWord.length) {
+            longestWord = array[i];
+        }
+    }
+    return longestWord;
+}
 
+export function sumArray(array) {
+    return array.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    }, 0);
+}
 
+export function averageNumbers(array) {
+    if (array.length === 0) {
+        return undefined;
+    }
+    let sum = array.reduce((accumulator, element) => accumulator + element, 0);
+    let average = sum / array.length;
+    return average;
+}
 
+export function averageWordLength(words) {
+    if (words.length === 0) {
+        return undefined;
+    }
+    let totalLength = words.reduce((accum, word) => accum + word.length, 0);
+    let average = totalLength / words.length;
+    return average;
+}
 
+export function uniquifyArray(array) {
+    if (array.length === 0) {
+        return undefined;
+    }
+    const uniqueArray = array.filter((item, index) => {
+        return array.indexOf(item) === index;
+    });
+    return uniqueArray;
+}
 
+export function doesWordExist(array, palabra) {
+    return array.includes(palabra);
+}
+
+export function howManyTimes(array, element) {
+    if (array.length === 0) {
+        return undefined;
+    }
+    const occurrences = array.filter(item => item === element);
+    return occurrences.length;
+}
+
+export function greatestProduct() { }
